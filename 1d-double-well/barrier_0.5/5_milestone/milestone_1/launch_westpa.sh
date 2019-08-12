2f25d6d9549285d97746fb5c31bccb1ea82e5da8
Dhiman Ray <53923981+dhimanray@users.noreply.github.com>
2019-08-12 15:46:29 -0700

    Add files via upload

---
+++ 1d-double-well/barrier_0.5/5_milestone/milestone_1/launch_westpa.sh
@@ -0,0 +1,5 @@
+#:/bin/bash
+
+#source /home/dhiman/weighted-ensemble/WESTPA/westpav2017.10/westpa/westpa.sh
+
+conda activate /home/dhiman/Miniconda2/envs/westpa-2017.10
---
+++ 1d-double-well/barrier_0.5/5_milestone/milestone_1/odld_system.py
@@ -0,0 +1,112 @@
+from __future__ import print_function, division
+import numpy
+from west.propagators import WESTPropagator
+from west.systems import WESTSystem
+from westpa.binning import RectilinearBinMapper
+
+PI = numpy.pi
+from numpy import sin, cos, exp
+from numpy.random import normal as random_normal
+
+pcoord_len = 21
+pcoord_dtype = numpy.float32    
+
+class ODLDPropagator(WESTPropagator):
+    def __init__(self, rc=None):
+        super(ODLDPropagator,self).__init__(rc)
+        
+        self.coord_len = pcoord_len
+        self.coord_dtype = pcoord_dtype
+        self.coord_ndim = 1
+        
+        self.initial_pcoord = numpy.array([-1.0], dtype=self.coord_dtype)  #Initial Position in x coordinate
+        
+        self.sigma = 0.001**(0.5)   #standard daviation of random noise
+        
+        self.A = 2
+        self.B = 10
+        self.C = 0.5
+        self.x0 = 1
+        
+        # Implement a reflecting boundary at this x value
+        # (or None, for no reflection)
+        self.reflect_at = 10.0
+        #self.reflect_at = None
+
+    def get_pcoord(self, state):
+        '''Get the progress coordinate of the given basis or initial state.'''
+        state.pcoord = self.initial_pcoord.copy()
+                
+    def gen_istate(self, basis_state, initial_state):
+        initial_state.pcoord = self.initial_pcoord.copy()
+        initial_state.istate_status = initial_state.ISTATE_STATUS_PREPARED
+        return initial_state
+
+    def propagate(self, segments):
+        
+        A, B, C, x0 = self.A, self.B, self.C, self.x0
+        
+        n_segs = len(segments)
+    
+        coords = numpy.empty((n_segs, self.coord_len, self.coord_ndim), dtype=self.coord_dtype)
+        
+        for iseg, segment in enumerate(segments):
+            coords[iseg,0] = segment.pcoord[0]
+            
+        twopi_by_A = 2*PI/A
+        half_B = B/2
+        sigma = self.sigma
+        gradfactor = self.sigma*self.sigma/2
+        coord_len = self.coord_len
+        reflect_at = self.reflect_at
+        all_displacements = numpy.zeros((n_segs, self.coord_len, self.coord_ndim), dtype=self.coord_dtype)
+
+        sink1 = -2.0
+        sink2 = 0.0
+        for istep in xrange(1,coord_len):
+            x = coords[:,istep-1,0]
+            
+            #xarg = twopi_by_A*(x - x0)
+            xarg = x
+            
+            eCx = numpy.exp(C*x)
+            eCx_less_one = eCx - 1.0
+           
+            all_displacements[:,istep,0] = displacements = random_normal(scale=sigma, size=(n_segs,))
+            #grad = half_B / (eCx_less_one*eCx_less_one)*(twopi_by_A*eCx_less_one*sin(xarg)+C*eCx*cos(xarg))
+            grad = -2.0*xarg*(1.0 - xarg*xarg)
+
+            newx = x.copy()
+
+            
+            #Absorb trajectories reaching end point
+            to_absorb1 = x > sink2
+            to_absorb2 =  x < sink1
+
+            #propagete all trajectories
+            newx = x - gradfactor*grad + displacements
+
+            #absorb trajectories which reached sink
+            newx[to_absorb1] = x[to_absorb1]
+            newx[to_absorb2] = x[to_absorb2]
+
+            coords[:,istep,0] = newx
+            
+        for iseg, segment in enumerate(segments):
+            segment.pcoord[...] = coords[iseg,:]
+            segment.data['displacement'] = all_displacements[iseg]
+            segment.status = segment.SEG_STATUS_COMPLETE
+    
+        return segments
+
+class ODLDSystem(WESTSystem):
+    def initialize(self):
+        self.pcoord_ndim = 1
+        self.pcoord_dtype = pcoord_dtype
+        self.pcoord_len = pcoord_len
+        
+        #self.bin_mapper = RectilinearBinMapper([[0,1.3] + list(numpy.arange(1.4, 10.1, 0.1)) + [float('inf')]])
+	self.bin_mapper = RectilinearBinMapper([ list(numpy.arange(-3.0, 3.0, 0.1)) ])  #bins for propagation
+        self.bin_target_counts = numpy.empty((self.bin_mapper.nbins,), numpy.int_)
+        self.bin_target_counts[...] = 20
+        
